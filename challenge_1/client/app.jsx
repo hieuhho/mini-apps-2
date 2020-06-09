573ed546e05abf68b4a5a2cd0150014d2d5b6474
Hieu Ho <hihho@ucdavis.edu>
2020-06-09 15:57:41 -0700

    React client

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -4,14 +4,42 @@ import ReactDOM from 'react-dom';
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      search: '',
+      data: null,
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  };
+
+  handleSearch(e) {
+    e.preventDefault();
+    const { text } = this.state;
+    fetch(`/events?q=${text}`)
+    .then(res => res.json())
+    .then(data => this.setState({data: data}))
   }
 
   render() {
+    const { data } = this.state;
     return (
       <div>
         <div>
-          hello world
+          <input type="text" id="search" onChange={this.handleChange} placeholder="search..."/>
+          <button type="button" onClick={this.handleSearch}>Search</button>
         </div>
+
+        {data && (
+          <div>{data}</div>
+        )}
+
       </div>
     )
   }
