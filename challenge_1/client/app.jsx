c428feb0b82ec8aaadd816e7f54846e7dc2f6cbf
Hieu Ho <hihho@ucdavis.edu>
2020-06-09 12:02:32 -0700

    init

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,20 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
 
+  render() {
+    return (
+      <div>
+        <div>
+          hello world
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
